5c7684a11a52899e39e72e232844b720423bc574
LiorG <liorgish@gmail.com>
2017-10-03 15:22:37 +0300

    mnu

--- scripts/home.js
+++ scripts/home.js
@@ -1,11 +1,116 @@
 (function () {
-$(document).ready(function () {
-    var obj=Object.create(PUBLIC_API);
-    obj.getData('http://noyaschleien.com/api/Data/GetTraverseItem',{"request":{"Language":"1"}}).then(function (data) {
-        console.log(data)
-    }).catch(function (err) {
+    $(document).ready(function () {
+        var obj = Object.create(PUBLIC_API);
 
+        function processData(index, card) {
+            var domManager = Object.create(DomManager);
+            var description = card.Description;
+            var id = card.ID;
+            var image_Url = card.Image_Url;
+            var text = card.Text;
+            var title = card.Title;
+
+            var a = 'item-wrp-' + index;
+            var data = {
+                id: a,
+                class: 'col-xs-12 col-md-4'
+            }
+
+            domManager.addElementToDom(index, 'div', data, 'cards-outer-wrp');
+
+            var b = 'item-row-wrp-' + index;
+            var data = {
+                id: b,
+                class: 'row item-wrp text-center'
+
+            }
+            domManager.addElementToDom(index, 'div', data, a);
+
+            var c = 'item-inner-wrp-' + index;
+            var data = {
+                id: c,
+                class: 'col-xs-12 text-center'
+
+            }
+            domManager.addElementToDom(index, 'div', data, b);
+
+            var d = 'item-content-wrp-' + index;
+            var data = {
+                id: d,
+                class: 'item-content',
+
+            }
+            domManager.addElementToDom(index, 'div', data, c);
+
+            function fb() {
+
+                $(this).fadeOut();
+                $('#' + g).fadeIn();
+
+            }
+
+            var e = 'close-btn-' + index;
+            var data = {
+                id: e,
+                class: 'item-content--toggle-state',
+                src: 'assets/close.ico',
+                css:{
+                    display:'none'
+                }
+
+            }
+            domManager.addElementToDom(index, 'img', data, d,'click',fb);
+
+
+            function fa() {
+
+                $(this).fadeOut();
+                $('#' + h).fadeIn();
+
+            }
+
+
+            var f = 'item-img-' + index;
+            var data = {
+                id: f,
+                class: 'item-content--img',
+                src: image_Url
+            }
+            domManager.addElementToDom(index, 'img', data, d, 'click', fa);
+
+
+
+
+            var g = 'item-content-state-' + index;
+            var data = {
+                id: g,
+                class: 'item-content--card-text',
+                html: text
+            }
+            domManager.addElementToDom(index, 'div', data, d);
+
+
+            var h = 'item-caption-' + index;
+            var data = {
+                id: h,
+                class: 'item-caption pointer',
+                html: title
+            }
+            domManager.addElementToDom(index, 'div', data, c);
+
+
+        }
+
+        obj.getData('http://noyaschleien.com/api/Data/GetTraverseItems', {"request": {"Language": "1"}}).then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.TraverseItems.length; i++) {
+                var card = data.TraverseItems[i];
+                processData(i, card)
+            }
+
+        }).catch(function (err) {
+
+        })
     })
-})
 
 }())
--- scripts/shared.js
+++ scripts/shared.js
@@ -1,18 +1,18 @@
 (function () {
     $(document).ready(function () {
-        jQuery.get('mnu.html', function(html) {
-          $('#mnu-place-holder').html(html)
+        jQuery.get('mnu.html', function (html) {
+            $('#mnu-place-holder').html(html)
         });
     })
 }())
-var PUBLIC_API =  {
+var PUBLIC_API = {
 
-    loadData :function (url, body) {
-        var data=JSON.stringify(body);
+    loadData: function (url, body) {
+        var data = JSON.stringify(body);
         var settings = {
             "async": true,
             "crossDomain": true,
-            "url":url,
+            "url": url,
             error: function (err) {
                 throw  err;
             },
@@ -20,7 +20,6 @@ var PUBLIC_API =  {
             "headers": {
                 "content-type": "application/json",
                 "cache-control": "no-cache",
-                "postman-token": "ab7054de-e5c0-db38-4fb1-1de548a424a2"
             },
             success: function (data) {
                 return data;
@@ -28,21 +27,28 @@ var PUBLIC_API =  {
             "processData": false,
             "data": data
         }
-        return    $.ajax(settings)
-
-
-
-
+        return $.ajax(settings)
     },
-    getData :function (url, body) {
-      return  this.loadData(url, body).then(function (data) {
-          return data;
-      }).catch(function (err) {
-          console.log(err);
-          return err;
-      })
+    getData: function (url, body) {
+        return this.loadData(url, body).then(function (data) {
+            return data;
+        }).catch(function (err) {
+            console.log(err);
+            return err;
+        })
     }
 
+}
+
+var DomManager = {
+    addElementToDom: function (index, name, data, where, event, func) {
+        var finalName = '<' + name + '/>'
+        var finalWhere = '#' + where;
+        var elem = $(finalName, data).appendTo(finalWhere);
+        if (typeof event !== "undefined" && typeof func !== "undefined")
+            elem.on(event, func);
+
+    }
 }
 
 
