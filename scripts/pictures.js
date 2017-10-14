1cd30c70157e72dcc571936826ab126f55b86cf4
gilior <liorgish@gmail.com>
2017-10-14 22:52:50 +0300

    pictures

--- scripts/pictures.js
+++ scripts/pictures.js
@@ -2,6 +2,8 @@
     var domManager = Object.create(DomManager);
     processImage = function (img) {
 
+
+        //set gallery
         var wrpID = 'img-wrp-' + img.ID;
         var divData = {
             id: wrpID,
@@ -18,9 +20,56 @@
             'data-target': '#myModal',
             src: img.ImageURL
         }
-        domManager.addElementToDom(1, 'img', imgData, wrpID)
+        domManager.addElementToDom(1, 'img', imgData, wrpID);
+
+
+
+
+
     }
+    processCrlImage=function (img) {
+        //set carousel
+        var crlWrpID='crl-wrp-id-'+img.ID;
+        var crlWrpData={
+            class:'item active',
+            id:crlWrpID
+        }
+        domManager.addElementToDom(1,'div',crlWrpData,'carousel-place-holder')
+
+
+        var crlImgData = {
+            id: 'crlImgData '+img.ID,
+            class: 'img-carousel img-responsive img-rounded',
+
+            src: img.ImageURL
+        }
+        domManager.addElementToDom(1, 'img', crlImgData, crlWrpID);
+
+        var crlTitleID='crl-title-id- '+img.ID;
+        var crlTitlwData={
+            class:'carousel-caption',
+            id:crlTitleID
+        }
+        domManager.addElementToDom(1,'div',crlTitlwData,crlWrpID)
+    }
+
     $(document).ready(function () {
+        jQuery.get('pictures-modal.html', function (html) {
+            $('#myModal').html(html);
+            api.getData('pictures',
+                'http://noyaschleien.com/api/Data/GetImages',
+                {"request": {"CurrentImageID": "2", "Language": "0", "NextData": "2", "DataAmount": "0"}})
+                .then(function (data) {
+                    var imgs = data.Images;
+                    for (var i = 0; i < imgs.length; i++) {
+                        var obj = imgs[i];
+                        processCrlImage(obj);
+                    }
+                })
+                .catch(function (err) {
+
+                })
+        });
         var api = Object.create(PUBLIC_API);
         api.getData('pictures',
             'http://noyaschleien.com/api/Data/GetImages',
